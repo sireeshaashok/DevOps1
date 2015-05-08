c3971b6f03a2f73f2a360922d18b38a590be6669
smallipedi <smallipedi@14.96.48.84>
2015-05-08 11:15:21 +0530

    checking TA1

--- DevOps/src/test/HelloWorld.java
+++ DevOps/src/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("test:::D444E4441:434r23435324324344:44435443434e32433:");
+		System.out.println("test:::D46456456546:");
 	}
 }
 
